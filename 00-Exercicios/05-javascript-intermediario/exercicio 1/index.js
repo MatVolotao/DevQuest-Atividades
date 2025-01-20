c8e02fd592dba6e9f18357249bba23999a2c175f
Matheus S. Volotão <matheus.volotao@hotmail.com>
2025-01-20 17:35:03 -0300

    exercicio 1 - com dois pensamentos diferentes

--- 00-Exercicios/05-javascript-intermediario/exercicio 1/index.js
+++ 00-Exercicios/05-javascript-intermediario/exercicio 1/index.js
@@ -1,3 +1,23 @@
+let estadoBotao = false
+function changeColor() {
+    if (!estadoBotao) {
+        document.getElementsByClassName("quadrado")[0].style.backgroundColor = "yellow";
+        estadoBotao = true
+    } else {
+        document.getElementsByClassName("quadrado")[0].style.backgroundColor = "blue";
+        estadoBotao = false
+    }
+}
+
+// OUTRO MODO DE FAZER e mais  organizado e escalável
+
+/* 
+        Passo 1 - Pegar o elemento do botão aqui no JS
+        Passo 2 - Pegar o elemento do quadrado azul aqui no JS
+        Passo 3 - Quando o usuário clicar no botão precisamos disparar a ação de mudar a cor do fundo do quadrado azul
+        Passo 4 - Para mudar a cor de fundo podemos adicionar e remover as classes que o elemento tem
+*/
+
 // Passo 1 - Pegar o elemento do botão aqui no JS
 const btnTrocaCor = document.getElementById("btn-troca-cor");
 
@@ -12,3 +32,4 @@ function trocarCor() {
   quadradoAzul.classList.remove("azul");
   quadradoAzul.classList.add("amarelo");
 }
+
